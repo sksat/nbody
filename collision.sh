d7237069e5398507562e974002a0cff1951bd17c
sksat <sksat.tec@gmail.com>
2018-08-07 20:40:45 +0900

    update

--- collision.sh
+++ collision.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-num=5000
+num=500
 
 rm *.dat
 ./mk_plummer -n $num > plummer-$num.dat
-./util.rb slide plummer-$num.dat  10.0  0.0  0.0 -1.0  0.0  0.0
+./util.rb slide plummer-$num.dat  3.0  0.5   0.0 -0.5  0.0  0.0
 mv plummer-$num-slide.dat plummer-$num-one.dat
-./util.rb slide plummer-$num.dat -10.0  0.0  0.0  1.0  0.0  0.0
+./util.rb slide plummer-$num.dat -3.0  -0.5  0.0  0.5  0.0  0.0
 ./util.rb merge plummer-$num-one.dat plummer-$num-slide.dat
 mv merged.dat plummer-collision-$num.dat
